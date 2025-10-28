4f453519bc2348735eb222c10d94674f541eea55
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-28 16:30:47 +0600

    added new folder

---
+++ HW_week2/HW_2.py
@@ -0,0 +1,11 @@
+bill = None
+while bill is None
+    try:
+	bill=float(input('enter the bill'))
+    except ValueError:
+	print("Error")
+
+tipsPercent = bill/10
+total = bill + tipsPercent
+
+print("Total bill: %.2f. Bill: %.2f. Tips: %.2f" % (total, bill, tipsPercent))
--- project/hello.py
+++ project/hello.py
@@ -51,18 +51,34 @@ and run Python code; now you just need to learn Python!
 
 # second task
 
-import sys
 
-# Define a main() function that prints a little greeting.
+# # Define a main() function that prints a little greeting.
+# def main():
+#   # Get the name from the command line, using 'World' as a fallback.
+#   if len(sys.argv) >= 2:
+#     name = sys.argv[1]
+#   else:
+#     name = 'World'
+#   print('Hello', name)
+
+# # This is the standard boilerplate that calls the main() function.
+# if __name__ == '__main__':
+#   main()
+
+
+# Donuts function
+# import sys
+
 def main():
-  # Get the name from the command line, using 'World' as a fallback.
-  if len(sys.argv) >= 2:
-    name = sys.argv[1]
+  num_don = int(input('How many donuts?'))
+  if num_don <= 9:
+    # num_don = sys.argv[1]
+    print("Number of donuts:", + num_don),
   else:
-    name = 'World'
-  print('Hello', name)
+    print("You eat too many of them")
 
-# This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
+# Код, который выполняется только при прямом запуске (например, main()),
+# и код (функции, классы), который можно использовать при импорте, не вызывая побочных эффектов.
+# «Если этот файл запущен напрямую (а не импортирован), то вызови функцию main()».
   main()
-
