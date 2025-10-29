c7823003007f8bf82aeb512b087974fe075a2e63
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-29 13:34:04 +0600

    Iterration HW_2

--- HW_week2/HW_2.py
+++ HW_week2/HW_2.py
@@ -1,12 +1,36 @@
-name = str(input('Please inser your name'))
-bill = None
-while bill is None:
-    try:
-        bill = float(input('enter the bill'))
-    except ValueError:
-        print("Error")
+# name = str(input('Please inser your name'))
+# bill = None
+# while bill is None:
+#     try:
+#         bill = float(input('enter the bill'))
+#     except ValueError:
+#         print("Error")
 
-tipsPercent = bill/10
-total = bill + tipsPercent
+# tipsPercent = bill/10
+# total = bill + tipsPercent
 
-print("Hello, %s. Your Bill is %.2f and the tips is %.2f. Total amount bill is %.2f" % (name, bill, tipsPercent, total))
+# print("Hello, %s. Your Bill is %.2f and the tips is %.2f. Total amount bill is %.2f" % (name, bill, tipsPercent, total))
+
+# -----------------
+# Call the above functions with interesting inputs
+
+def main():
+    print('match_ends')
+    test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 1)
+    test(match_ends(['', 'x', 'xy', 'xyx', 'xx]), 2)
+    test(match_ends(['aaa', 'be', 'abc', 'hello']), 3)
+
+    # print()
+    # print('front_x')
+    # test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']), ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+    # test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']), ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+    # test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']), ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+    # print()
+    # print('sort_last')
+    # test(sort_last([(1, 3), (3, 2), (2, 1)]), [(2, 1), (3, 2), (1, 3)])
+    # test(sort_last([(2, 3), (1, 2), (3, 1)]), [(3, 1), (1, 2), (2, 3)])
+    # test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]), [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+    
+    # if __name__ == '__main__':
+main()
