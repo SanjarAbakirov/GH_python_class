777ecc6ad099a8d6ce1e542289554c3eeb753fb7
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-29 11:59:13 +0600

    changing places total

--- HW_week2/HW_2.py
+++ HW_week2/HW_2.py
@@ -1,12 +1,11 @@
 bill = None
 while bill is None:
     try:
-	    bill=float(input('enter the bill'))
+	    bill = float(input('enter the bill'))
     except ValueError:
-		
-	    print("Error")
+        print("Error")
 
 tipsPercent = bill/10
 total = bill + tipsPercent
 
-print("Total bill: %.2f. Bill: %.2f. Tips: %.2f" % (total, bill, tipsPercent))
+print("Total bill: %.2f. Bill: %.2f. Tips: %.2f" % (bill, tipsPercent, total))
