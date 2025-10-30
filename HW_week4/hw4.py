0756cc06a1c8a9ec589bb010daff69ebe353ceb7
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-30 14:24:31 +0600

    auto commit Thu Oct 30 14:24:31 +06 2025

---
+++ HW_week4/hw4.py
@@ -0,0 +1,17 @@
+# Bill Calculator with people's names
+people_list = []
+
+name = str(input('Please insert your name'))
+people =str(input('Please insert name of participants separated by comma'))
+
+bill = None
+while bill is None:
+    try:
+        bill = float(input('enter the bill'))
+    except ValueError:
+        print("Error")
+
+tipsPercent = bill/10
+total = bill + tipsPercent
+
+print("Hello, %s. Your Bill is %.2f and the tips is %.2f. Total amount bill is %.2f" % (name, bill, tipsPercent, total))
