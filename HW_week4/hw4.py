4dc4198d987c84976aadd5e71caa0c5742fe0ba2
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-31 14:33:32 +0600

    auto commit Fri Oct 31 14:33:32 +06 2025

--- HW_week4/hw4.py
+++ HW_week4/hw4.py
@@ -17,7 +17,7 @@ while bill is None:
 tipsPercent = bill/10
 total = bill + tipsPercent
 
-# print("Hello, %s. Your Bill is %.2f and the tips is %.2f. Total amount bill is %.2f" % (name, bill, tipsPercent, total))
+print("Hello, %s. Your Bill is %.2f and the tips is %.2f. Total amount bill is %.2f" % (name, bill, tipsPercent, total))
 
 
 # Lesson #4 logical operators
