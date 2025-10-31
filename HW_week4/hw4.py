c78e99295d3d838ae42e96f2e5c0c93b43853c44
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-31 12:55:34 +0600

    auto commit Fri Oct 31 12:55:34 +06 2025

--- HW_week4/hw4.py
+++ HW_week4/hw4.py
@@ -4,7 +4,7 @@ people_list = []
 # name = str(input('Please insert your name'))
 people = str(input('Please insert name of participants'))
 people_list.append(people)
-words = people_list[0].split(',')
+words = people_list[0].split()
 print(words)
 
 
