6207904614823539e5f92439ccfdbc5c23e97d03
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-31 12:15:18 +0600

    auto commit Fri Oct 31 12:15:18 +06 2025

--- HW_week4/hw4.py
+++ HW_week4/hw4.py
@@ -2,7 +2,9 @@
 # people_list = []
 
 # name = str(input('Please insert your name'))
-# people =str(input('Please insert name of participants'))
+people = str(input('Please insert name of participants'))
+
+print(yourFriendsNames = people.split(','))
 
 # bill = None
 # while bill is None:
@@ -20,9 +22,9 @@
 # Lesson #4 logical operators
 
 # boolean
-x = 2
-print(x == 2) # prints true by default
-print(x == 3) # prints false by default
-print(x < 3) # prints true by default
+# x = 2
+# print(x == 2) # prints true by default
+# print(x == 3) # prints false by default
+# print(x < 3) # prints true by default
 
 
