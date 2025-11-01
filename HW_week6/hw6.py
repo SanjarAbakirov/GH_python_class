b9f0a14e66444d98c173380eb96bf2fb2ae5b1e1
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-02 00:52:48 +0600

    auto commit Sun Nov  2 00:52:48 +06 2025

--- HW_week6/hw6.py
+++ HW_week6/hw6.py
@@ -1,8 +1,9 @@
 from collections import Counter
 # str ="My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which, if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges."
-str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+# str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 
-
+str = 0
+sentence = str(input("Enter your sentence: "))
 
 
 list = str.split()
