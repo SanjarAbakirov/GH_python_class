940700f1fa4ff59d99a12e96fa3a086b06b11c1b
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-02 17:55:48 +0600

    auto commit Sun Nov  2 17:55:48 +06 2025

--- HW_week2/HW_2.py
+++ HW_week2/HW_2.py
@@ -1,141 +1,122 @@
-# name = str(input('Please inser your name'))
-# bill = None
-# while bill is None:
-#     try:
-#         bill = float(input('enter the bill'))
-#     except ValueError:
-#         print("Error")
+# A match_ends
 
-# tipsPercent = bill/10
-# total = bill + tipsPercent
+# words = ["level", "property", "aza", "kazak", "python", "lesha", "amanda", "asa", "sas", "s", "de"]
+# def match_ends(words):
+#   arr = []
+#   count = 0
+#   for word in words:
+#       if len(word) >= 2:
+#          count += 1 
+#   return count
+# match_ends(words)
+# result = match_ends(words)
+# print(result)
 
-# print("Hello, %s. Your Bill is %.2f and the tips is %.2f. Total amount bill is %.2f" % (name, bill, tipsPercent, total))
+# training
+# words = ["xylophone", "apple", "axe", "extra", "berry", "xenon"]
+# result = 0
+# def gpt_fn(words):
+#     arr = []
+#     for word in words:
+#       if word[0] == "x" or len(word) < 5:
+#          arr.append(word)
+#     return arr
+# gpt_fn(words)
+# result = gpt_fn(words)
+# print(result)
 
-# -----------------
-# Call the above functions with interesting inputs
+# Task - the palindrome
+# words = ["level", "property", "aza", "kazak", "python", "lesha", "amanda", "kyshtak", "sas", "xex"]
 
+# def match_ends(words):
+#   arr = []
+#   for word in words:
+#       if word[0] == word[-1]:
+#         arr.append(word)
+#   return arr
+# match_ends(words)
+# result = match_ends(words)
+# print(result)
+
+# task front_x
+# words = ['mix', 'xyz', 'apple', 'xanadu', 'aardvark', 'java', 'xawier', 'avitchi', 'xpara']
+# result = 0
+
+# def front_x(words):
+#   arr = []
+#   arr2 = []
+#   joined = 0
+#   for word in words:
+#       if word[0] == 'x':
+#         arr.append(word)
+#       else:
+#          arr2.append(word)
+#       joined = arr + arr2
+#   return joined
+# front_x(words)       
+# result = front_x(words)
+# print(result)
+
+
+# sort_last
+# list = [(1, 7), (1, 3), (3, 4, 5), (2, 2)]
+# element = lambda element:element[1]
+# list.sort(key=element)
+# print(list)
+
+# test fn (the first way)
+
+def main(n):
+    return n ** 3
+import unittest
+
+class MyTest(unittest.TestCase):
+  got = main(5)
+  expected = 125
+  def test(self):
+    self.assertEqual(self.got, self.expected)
+    if self.got == self.expected:
+        prefix = 'Ok'
+    else:
+        prefix = 'X'
+    print('%s got: %s expected: %s' % (prefix, repr(self.got), repr(self.expected)))
+
+if __name__ == '__main__':
+    unittest.main()
+ 
+
+
+
+# test fn (the second way)
 # def main():
-#     print('match_ends')
-#     test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 1)
-#     test(match_ends(['', 'x', 'xy', 'xyx', 'xx]), 2)
-#     test(match_ends(['aaa', 'be', 'abc', 'hello']), 3)
-
-    # print()
-    # print('front_x')
-    # test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']), ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
-    # test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']), ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
-    # test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']), ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
-
-    # print()
-    # print('sort_last')
-    # test(sort_last([(1, 3), (3, 2), (2, 1)]), [(2, 1), (3, 2), (1, 3)])
-    # test(sort_last([(2, 3), (1, 2), (3, 1)]), [(3, 1), (1, 2), (2, 3)])
-    # test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]), [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
-    
-    # if __name__ == '__main__':
+#     def test(got, expected):
+#         if got == expected:
+#             prefix = 'Ok'
+#         else:
+#             prefix = 'X'
+#         print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+#     test()
 # main()
 
 
 
-# -----My solution---------------
-# Calls the above functions with interesting inputs
-
-# match-end
-# words = ['aba', 'xyz', 'aa', 'x', 'bbb']
-# words = ['', 'x', 'xy', 'xyx', 'xx']
-# words = ['aaa', 'be', 'abc', 'hello']
-
-# 'front_x'
-# words = ['bbb', 'ccc', 'axx', 'xzz', 'xaa']
-# words = ['ccc', 'bbb', 'aaa', 'xcc', 'xaa']
-# words = ['mix', 'xyz', 'apple', 'xanadu', 'aardvark']
-
-# 'sort_last'
-# tuple = [(1, 3), (3, 2), (2, 1)]
-# tuple = [(2, 3), (1, 2), (3, 1)]
-tuple = [(1, 7), (1, 3), (3, 4, 5), (2, 2)]
-
-# matching stage Match_end
-# def match_end(words):
-#     count = 0
-#     for word in words:
-#         if len(word) >= 3 and word[0] == word[-1]:
-#             count += 1
-#     return count
-#     # return sum(1 for word in words if len(word) >= 3 and word[0] == word[-1])
-
-# matching stage Front_x
-# def front_x(words):
-#     listX = []
-#     list = []
-#     for word in words:
-#         if word[0] == 'x':
-#             listX.append(word)
-#         else:
-#             list.append(word)
-#     listX.sort()
-#     list.sort()
-#     return listX + list
-
-# matching stage Sort_last
-def sort_last(tuple):
-    num = sorted(tuple, key=lambda x: x[-1])
-    return num
-
-# testing stage
-def test(got, expected):
-    if got == expected:
-        print(f"Test passed: Got is {got}, Expected {expected}")
-    else:
-        print(f"Test is not passed: Got is {got}, Expected {expected}")
-
-# input stage
-def main():
-    # test(match_end(words), 3)
-    # test(match_end(words), 2)
-    # test(match_end(words), 1)
-    # test(front_x(words), ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
-    # test(front_x(words), ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
-    # test(front_x(words), ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
-    # test(sort_last(tuple), [(2, 1), (3, 2), (1, 3)])
-    # test(sort_last(tuple), [(3, 1), (1, 2), (2, 3)])
-    test(sort_last(tuple), [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
-main()
-
-
-# this is too early method
-# print(list(sorted(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])))
-
-# lst = ['mix', 'xyz', 'apple', 'xanadu', 'aardvark', 'xerox']
-# lst.sort()
-# print(lst)
-
-
-
-# -------Just practice -------
-# a = str(input("insert Name"))
-# b = int(input("insert integer"))
-# c= float(input("insert a float number"))
-# result = None
-
-# def testFn(a, b, c):
-#     f = b + c
-#     return f
-# result = testFn(a, b, c)
-# print("Hello %s, your result is %.2f" % (a, result))
-
-# -----------------
-
-# def count(n):
-#     if n == 0:
-#         print('Engine!')
-#     else:
-#         print(n)
-#         count(n-1)
-# count(10)
-
-# -----------------
 
 
 
 
+
+
+# def sum_num(a, b):
+#    return a + b
+
+
+# practice
+
+# num_don = int(input("How many donuts?"))
+# # print(type(num_don))
+# if num_don >10:
+#     print("A lot. Eat less")
+# elif num_don == 0: 
+#     print("find more donuts")
+# else:
+#     print("You have enough")
