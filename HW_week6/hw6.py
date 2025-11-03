bfaad2a43ea076afaa3faadd5bbdb967f6f97c3a
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-03 22:02:46 +0600

    auto commit Mon Nov  3 22:02:46 +06 2025

--- HW_week6/hw6.py
+++ HW_week6/hw6.py
@@ -2,9 +2,9 @@ from collections import Counter
 # str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 
 sentence = str(input("Enter your sentence: "))
-print(sentence.split())
+# print(sentence.split())
 splitedSentece = Counter(sentence.split())
-
+print(splitedSentece)
 dictionary = dict(splitedSentece)
 # print(dictionary)
 
