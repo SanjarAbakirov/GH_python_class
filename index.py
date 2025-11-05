8ee514ed0829707065ad797a0aa6423c4961d222
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-05 17:54:55 +0600

    Added code for requests

--- index.py
+++ index.py
@@ -3,3 +3,14 @@
 # unit_price = 3
 
 # print("Hello")
+
+import requests
+
+# Выполняем GET-запрос к указанному URL
+res = requests.get('https://skillbox.ru')
+
+# Печатаем код состояния ответа. 200 означает успешное выполнение запроса.
+print(res.status_code)
+
+# Можно также вывести сам объект ответа
+print(res)
