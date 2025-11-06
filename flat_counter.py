f62659eacec4c7dc9a7f141ae19b3500445dc259
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-06 16:23:55 +0600

    auto commit Thu Nov  6 16:23:55 +06 2025

--- flat_counter.py
+++ flat_counter.py
@@ -1,4 +1,4 @@
-flat_number = int(insert("add number of the flat:"))
+flat_number = int(input("add number of the flat:"))
 
 entrance_number = (flat_number - 1) // 20 + 1
 floor_number = (flat_number - 1) % 20 // 4 + 1
