315459b7e25b025b0c7474f2a2d57cd570058728
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-06 23:36:07 +0600

    auto commit Thu Nov  6 23:36:07 +06 2025

--- proIt/string.py
+++ proIt/string.py
@@ -1,20 +1,30 @@
-my_string = "Hi I am Sam"
-his_string = 'He he is Tom'
+# my_string = "Hi I am Sam"
+# his_string = 'He he is Tom'
 
-# print(my_string)
-# print(his_string)
+# # print(my_string)
+# # print(his_string)
 
-big_text = "В математике повсеместно используются символы для упрощения и сокращения текста. Ниже приведён список наиболее часто встречающихся математических обозначений, соответствующие команды в TeX, объяснения и примеры использования. Список и смысл обозначений соответствует международным стандартам ISO 31-11 и ISO 80000-2[1]"
-# print(big_text)
+# big_text = "В математике повсеместно используются символы для упрощения и сокращения текста. Ниже приведён список наиболее часто встречающихся математических обозначений, соответствующие команды в TeX, объяснения и примеры использования. Список и смысл обозначений соответствует международным стандартам ISO 31-11 и ISO 80000-2[1]"
+# # print(big_text)
 
-first_name = "Alice"
-last_name = "Smith"
-full_name = first_name + " " + last_name
-# print(full_name)
+# first_name = "Alice"
+# last_name = "Smith"
+# full_name = first_name + " " + last_name
+# # print(full_name)
 
-# length = len(full_name)
-# print(length)
+# # length = len(full_name)
+# # print(length)
 
-integer = 100
-string = str(integer)
-# print(type(my_string))  # type - depicts what is the class
+# integer = 100
+# # string = str(integer)
+# # print(type(my_string))  # type - depicts what is the class
+
+# string = int('10')
+# print(string)
+# # whten we take the num from the input
+
+
+# ---big int -----
+
+bit_integer = 2 ** 1000
+print(len(str(bit_integer)))
