f806c897c4a43b918ca450863e1881bd43e7e461
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-06 23:16:00 +0600

    auto commit Thu Nov  6 23:16:00 +06 2025

--- proIt/boolean.py
+++ proIt/boolean.py
@@ -55,3 +55,11 @@
 
 
 # --leap year -------
+
+year = 2000
+
+if year % 4 == 0 and year % 100 != 0:
+    print("Leap year")
+# здесь от пройдет без остатка но на 100 без остатака не разделится
+elif year % 400 == 0:
+    print("year is leap")
