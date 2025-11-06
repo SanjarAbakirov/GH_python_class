c2d68413de89563b76901a198551af8d91c457e8
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-06 17:07:48 +0600

    auto commit Thu Nov  6 17:07:48 +06 2025

--- proIt/boolean.py
+++ proIt/boolean.py
@@ -2,10 +2,23 @@ is_student = True
 is_graduated = False
 # print(is_student)
 # ----------------------
-x = 10
-y = 10
+# x = 10
+# y = 10
 # print(x == y)
-is_equal = x != y
+# is_equal = x != y
 # print(is_equal)
 # print(x < 5 and x > 2)
 # ----------------------
+
+# if True:
+# print('Hello!')
+
+# ----------------------
+
+x = 0
+if x > 0:
+    print("x is positive")
+elif x < 0:
+    print("x is negative")
+else:
+    print("x is zero")
