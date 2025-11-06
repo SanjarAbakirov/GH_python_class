618034c7e85de2d75272298479ec39101be15d6d
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-06 16:43:42 +0600

    auto commit Thu Nov  6 16:43:42 +06 2025

---
+++ proIt/boolean.py
@@ -0,0 +1,7 @@
+is_student = True
+is_graduated = False
+print(is_student)
+
+x = 10
+y = 10
+print)x == y
--- proIt/flat_counter.py
+++ proIt/flat_counter.py
@@ -5,9 +5,10 @@ flat_number = int(input("add number of the flat:"))
 # floor_number = (flat_number - 1) % 20 // 4 + 1
 # остаток от делдения кв деленная на кол во кв водном подьезде, 4 кв в подькзде
 
+# Genka
 entrance_number = (flat_number - 1) // 8 + 1
 floor_number = (flat_number - 1) % 8 // 2 + 1
 
 
-print("Подъезд №: %d")(entrance_number)
-print("Этаж №:" + floor_number)
+print("Подъезд №: %d" % (entrance_number))
+print("Этаж №: %d" % (floor_number))
