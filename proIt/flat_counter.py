1b4160eea69c222600b591e465294c38d251aa49
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-06 16:31:37 +0600

    auto commit Thu Nov  6 16:31:37 +06 2025

--- proIt/flat_counter.py
+++ proIt/flat_counter.py
@@ -3,6 +3,7 @@ flat_number = int(input("add number of the flat:"))
 
 entrance_number = (flat_number - 1) // 20 + 1
 floor_number = (flat_number - 1) % 20 // 4 + 1
+# остаток от делдения кв деленная на кол во кв водном подьезде
 
 print(entrance_number)
 print(floor_number)
