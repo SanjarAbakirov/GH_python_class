7d5d82a6436d1281589d279bb5fb342eafa75d30
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-09 23:31:58 +0600

    auto commit Sun Nov  9 23:31:57 +06 2025

--- HW_week7/math_iteration.py
+++ HW_week7/math_iteration.py
@@ -12,5 +12,13 @@
 
     print(is_prime(17))  # True
 
+# Problem. Is there a positive integer that is divisible by  and ends with ?
 
+# To prove that such a number exists, it is enough to give a single example. 
+# One such example is : it ends with  and it is divisible by  (). 
+# This already proves the existence, and we don't even need to explain how we have found this integer. 
+# Still, the following three lines of code help to find all such integers in the range .
 
+for n in range(10 ** 4):
+    if n % 13 == 0 and n % 100 == 15:
+        print(n)
