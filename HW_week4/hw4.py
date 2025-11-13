8d5d9da5cebcf3d8e26f732fb51d8cc0b5ff127b
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-13 22:22:43 +0600

    auto commit Thu Nov 13 22:22:43 +06 2025

--- HW_week4/hw4.py
+++ HW_week4/hw4.py
@@ -1,9 +1,9 @@
 #  smth about debugging
 
 # the algorythm for debugging
-c = 0
-a = 33
-b = 200
-if b > a:
-    print("b is greater than a")
-    print(b)
+# c = 0
+# a = 33
+# b = 200
+# if b > a:
+#     print("b is greater than a")
+#     print(b)
