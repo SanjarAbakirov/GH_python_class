aef8bb95e9e017250d7b9f0cdadc7dbb91917109
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-13 23:42:28 +0600

    auto commit Thu Nov 13 23:42:28 +06 2025

--- test/test_calk.py
+++ test/test_calk.py
@@ -6,8 +6,12 @@ class TestCalc(unittest.TestCase):
 
     def test_add(self):
         # docs.python.org/3/library/unittest.py
-        result = test.add(10, 5)
-        self.assertEqual(result, 14)
+        # result = test.add(10, 5)
+        # self.assertEqual(result, 15)
+        self.assertEqual(test.add(10, 5), 15)
+        self.assertEqual(test.add(1, -1), 1)
+        self.assertEqual(test.add(-1, -1), -2)
+        self.assertEqual(test.add(10, 9), 0)
 
 
 if __name__ == '__main__':  # run code within conditional - with unitttest.main
