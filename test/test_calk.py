a4b538ae8834f127545cc4fb51126961e14e7cd2
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-15 01:06:45 +0600

    auto commit Sat Nov 15 01:06:45 +06 2025

--- test/test_calk.py
+++ test/test_calk.py
@@ -3,20 +3,20 @@ import test
 
 
 class TestCalc(unittest.TestCase):
+    # def test_add(self):
+    # docs.python.org/3/library/unittest.py
+    # result = test.add(10, 5)
+    # self.assertEqual(result, 15)
 
     def test_add(self):
-        # docs.python.org/3/library/unittest.py
-        # result = test.add(10, 5)
-        # self.assertEqual(result, 15)
-        self.assertEqual(test.add(10, 5), 15)
-        self.assertEqual(test.add(1, -1), 0)
+        self.assertEqual(test.add(15, -15), 0)
         self.assertEqual(test.add(-1, -1), -2)
         self.assertEqual(test.add(10, 9), 19)
 
-    def test_add(self):
-        self.assertEqual(test.add(1, -1), 0)
-        self.assertEqual(test.add(-1, -1), -2)
-        self.assertEqual(test.add(10, 9), 19)
+    def test_subtract(self):
+        self.assertEqual(test.subtract(1, -1), 0)
+        self.assertEqual(test.subtract(-1, -1), -2)
+        self.assertEqual(test.subtract(10, 9), 19)
 
     def test_add(self):
         self.assertEqual(test.add(1, -1), 0)
