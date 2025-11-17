184f040567ecf0f0845c5bb43b16f00105991732
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-11-17 22:27:36 +0600

    auto commit Mon Nov 17 22:27:36 +06 2025

--- proIt/temp.py
+++ proIt/temp.py
@@ -1,5 +1,5 @@
 
-fahrenheit = int(input('Please insert temperature'))
+fahrenheit = float(input('Please insert temperature'))
 # fahrenheit = 68
 celsius = (5/9) * (fahrenheit - 32)
 print("Температура в Цельсиях:", celsius)
