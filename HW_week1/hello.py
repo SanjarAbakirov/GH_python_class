88663fbcafab6021c6a213cf15608f37fa49abd0
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-04 23:30:45 +0600

    some changes

--- HW_week1/hello.py
+++ HW_week1/hello.py
@@ -10,8 +10,8 @@
 
 # x = 1
 # if x == 1:
-    # intended four spaces
-    # print("x is 1")
+# intended four spaces
+# print("x is 1")
 
 # Task_2 Alice
 
@@ -20,6 +20,7 @@ def greetings_Alice():
 # greetings_Alice()
 
 # Task 3 change "Hello" for "Howdy"
+
 
 """A tiny Python program to check that Python is working. Try running this program from the command line like this:
   python hello.py
@@ -32,19 +33,23 @@ and run Python code; now you just need to learn Python!
 """
 
 # def greet_fn(name, extra_word):
-    # print("Hello, " + name + ". Welcome and " + extra_word)
+# print("Hello, " + name + ". Welcome and " + extra_word)
 
 # greet_fn("Sam", "Have a good day")
 
 # first way
-# def function_alice(greeting, name):
-    # print("%s, %s"%(greeting, name))
-# function_alice("Hello", "Alice")
-# function_alice("Howdy", "Alice")
+
+
+def function_alice(greeting, name):
+    print("%s, %s" % (greeting, name))
+
+
+function_alice("Hello", "Alice")
+function_alice("Howdy", "Alice")
 
 # second way
 # def fn_greet_alice(greeting, name):
-    # print(greeting + ". " + name)
+# print(greeting + ". " + name)
 # fn_greet_alice("Hello", "Alice")
 # fn_greet_alice("Howdy", "Alice")
 
@@ -68,15 +73,16 @@ and run Python code; now you just need to learn Python!
 # import sys
 
 def main():
-  num_don = int(input('How many donuts?'))
-  if num_don <= 9:
-    # num_don = sys.argv[1]
-    print("Number of donuts:", + num_don),
-  else:
-    print("You eat too many of them")
+    num_don = int(input('How many donuts?'))
+    if num_don <= 9:
+        # num_don = sys.argv[1]
+        print("Number of donuts:", + num_don),
+    else:
+        print("You eat too many of them")
+
 
 if __name__ == '__main__':
-# Код, который выполняется только при прямом запуске (например, main()),
-# и код (функции, классы), который можно использовать при импорте, не вызывая побочных эффектов.
-# «Если этот файл запущен напрямую (а не импортирован), то вызови функцию main()».
-  main()
+    # Код, который выполняется только при прямом запуске (например, main()),
+    # и код (функции, классы), который можно использовать при импорте, не вызывая побочных эффектов.
+    # «Если этот файл запущен напрямую (а не импортирован), то вызови функцию main()».
+    main()
