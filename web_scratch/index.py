3a4cd822b76aaef97cca98493257da3f9de64624
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-07 22:02:55 +0600

    auto commit Sun Dec  7 22:02:55 +06 2025

--- web_scratch/index.py
+++ web_scratch/index.py
@@ -2,3 +2,5 @@ from urllib.request import urlopen
 url = "http://olympus.realpython.org/profiles/aphodite"
 
 page = urlopen(url)  # urlopen() returns HTTP responce onject
+
+# page < http.client.HTTPResponse object at 0x150fef820 >
