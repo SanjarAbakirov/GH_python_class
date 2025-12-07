8247e019100d10aa86394027592b3307d02dd79e
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-07 22:42:13 +0600

    auto commit Sun Dec  7 22:42:13 +06 2025

--- web_scratch/index.py
+++ web_scratch/index.py
@@ -9,6 +9,7 @@ html_bytes = page.read()  # returns a sequence bytes
 html = html_bytes.decode("utf-8")  # convert to the str format
 
 print(html)
+
 <html >
 <head >
 <title > Profile: Aphrodite < /title >
