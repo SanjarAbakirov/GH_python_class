82ec5a8b5821c16dc5c05dcb238f77bbf9c15516
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-12-08 22:37:28 +0600

    auto commit Mon Dec  8 22:37:28 +06 2025

--- web-scraper/index.py
+++ web-scraper/index.py
@@ -3,7 +3,7 @@ url = "http://olympus.realpython.org/profiles/aphodite"
 
 page = urlopen(url)  # urlopen() returns HTTP responce onject
 
-# page < http.client.HTTPResponse object at 0x150fef820 >
+# page < hsttp.client.HTTPResponse object at 0x150fef820 >
 
 html_bytes = page.read()  # returns a sequence bytes
 html = html_bytes.decode("utf-8")  # convert to the str format
@@ -11,7 +11,7 @@ html = html_bytes.decode("utf-8")  # convert to the str format
 print(html)
 html = """
 <html>
-<head>
+<head>ss
 <title> Profile: Aphrodite </title >
 </head>
 <body bgcolor = "yellow">
